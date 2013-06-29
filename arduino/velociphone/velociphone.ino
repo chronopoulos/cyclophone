a137fa3023d8aa8ab555e03fef8d4da36ad0bf2a
B <bburdette@Parvis.(none)>
2013-06-29 09:08:42 -0600

    hopefully fast 'velociphone' sketch.  compiles but not tested yet.

--- arduino/velociphone/velociphone.ino
+++ arduino/velociphone/velociphone.ino
@@ -1,5 +1,4 @@
 
-
 long debounceDelay = 20;
 
 int knobPin0 = A0;
@@ -14,11 +13,167 @@ int knobPin2 = A2;
 int knobVal2 = 0;
 int prevKnobVal2 = 0;
 
+class PinChecker
+{
+public:
+  PinChecker(volatile unsigned char *aRegister, char bitindex)
+  {
+    mRegister = aRegister;
+    mask = 0x01;
+    mask <<= bitindex;
+  }
+
+  volatile unsigned char *mRegister;
+  unsigned char mask; 
+
+  inline bool isPinOn() const { return (*mRegister & mask) != 0x00; }
+  // inline bool isPinOn() const { return true; }
+
+};
+
+
+PinChecker PinCheckers[54] = {
+  PinChecker(&PINE, 0),
+  PinChecker(&PINE, 1),
+  PinChecker(&PINE, 4),
+  PinChecker(&PINE, 5),
+  PinChecker(&PING, 5),
+  PinChecker(&PINE, 3),
+  PinChecker(&PINH, 3),
+  PinChecker(&PINH, 4),
+  PinChecker(&PINH, 5),
+  PinChecker(&PINH, 6),
+  PinChecker(&PINB, 4),
+  PinChecker(&PINB, 5),
+  PinChecker(&PINB, 6),
+  PinChecker(&PINB, 7),
+  PinChecker(&PINJ, 1),
+  PinChecker(&PINJ, 0),
+  PinChecker(&PINH, 1),
+  PinChecker(&PINH, 0),
+  PinChecker(&PIND, 3),
+  PinChecker(&PIND, 2),
+  PinChecker(&PIND, 1),
+  PinChecker(&PIND, 0),
+  PinChecker(&PINA, 0),
+  PinChecker(&PINA, 1),
+  PinChecker(&PINA, 2),
+  PinChecker(&PINA, 3),
+  PinChecker(&PINA, 4),
+  PinChecker(&PINA, 5),
+  PinChecker(&PINA, 6),
+  PinChecker(&PINA, 7),
+  PinChecker(&PINC, 7),
+  PinChecker(&PINC, 6),
+  PinChecker(&PINC, 5),
+  PinChecker(&PINC, 4),
+  PinChecker(&PINC, 3),
+  PinChecker(&PINC, 2),
+  PinChecker(&PINC, 1),
+  PinChecker(&PINC, 0),
+  PinChecker(&PIND, 7),
+  PinChecker(&PING, 2),
+  PinChecker(&PING, 1),
+  PinChecker(&PING, 0),
+  PinChecker(&PINL, 7),
+  PinChecker(&PINL, 6),
+  PinChecker(&PINL, 5),
+  PinChecker(&PINL, 4),
+  PinChecker(&PINL, 3),
+  PinChecker(&PINL, 2),
+  PinChecker(&PINL, 1),
+  PinChecker(&PINL, 0),
+  PinChecker(&PINB, 3),
+  PinChecker(&PINB, 2),
+  PinChecker(&PINB, 1),
+  PinChecker(&PINB, 0)
+};
+
+
+/*
+void initPinCheckers()
+{
+	PinCheckers[0] = new PinChecker(&PINE, 0);
+	PinCheckers[1] = new PinChecker(&PINE, 1);
+	PinCheckers[2] = new PinChecker(&PINE, 4);
+	PinCheckers[3] = new PinChecker(&PINE, 5);
+	PinCheckers[4] = new PinChecker(&PING, 5);
+	PinCheckers[5] = new PinChecker(&PINE, 3);
+	PinCheckers[6] = new PinChecker(&PINH, 3);
+	PinCheckers[7] = new PinChecker(&PINH, 4);
+	PinCheckers[8] = new PinChecker(&PINH, 5);
+	PinCheckers[9] = new PinChecker(&PINH, 6);
+	PinCheckers[10] = new PinChecker(&PINB, 4);
+	PinCheckers[11] = new PinChecker(&PINB, 5);
+	PinCheckers[12] = new PinChecker(&PINB, 6);
+	PinCheckers[13] = new PinChecker(&PINB, 7);
+	PinCheckers[14] = new PinChecker(&PINJ, 1);
+	PinCheckers[15] = new PinChecker(&PINJ, 0);
+	PinCheckers[16] = new PinChecker(&PINH, 1);
+	PinCheckers[17] = new PinChecker(&PINH, 0);
+	PinCheckers[18] = new PinChecker(&PIND, 3);
+	PinCheckers[19] = new PinChecker(&PIND, 2);
+	PinCheckers[20] = new PinChecker(&PIND, 1);
+	PinCheckers[21] = new PinChecker(&PIND, 0);
+	PinCheckers[22] = new PinChecker(&PINA, 0);
+	PinCheckers[23] = new PinChecker(&PINA, 1);
+	PinCheckers[24] = new PinChecker(&PINA, 2);
+	PinCheckers[25] = new PinChecker(&PINA, 3);
+	PinCheckers[26] = new PinChecker(&PINA, 4);
+	PinCheckers[27] = new PinChecker(&PINA, 5);
+	PinCheckers[28] = new PinChecker(&PINA, 6);
+	PinCheckers[29] = new PinChecker(&PINA, 7);
+	PinCheckers[30] = new PinChecker(&PINC, 7);
+	PinCheckers[31] = new PinChecker(&PINC, 6);
+	PinCheckers[32] = new PinChecker(&PINC, 5);
+	PinCheckers[33] = new PinChecker(&PINC, 4);
+	PinCheckers[34] = new PinChecker(&PINC, 3);
+	PinCheckers[35] = new PinChecker(&PINC, 2);
+	PinCheckers[36] = new PinChecker(&PINC, 1);
+	PinCheckers[37] = new PinChecker(&PINC, 0);
+	PinCheckers[38] = new PinChecker(&PIND, 7);
+	PinCheckers[39] = new PinChecker(&PING, 2);
+	PinCheckers[40] = new PinChecker(&PING, 1);
+	PinCheckers[41] = new PinChecker(&PING, 0);
+	PinCheckers[42] = new PinChecker(&PINL, 7);
+	PinCheckers[43] = new PinChecker(&PINL, 6);
+	PinCheckers[44] = new PinChecker(&PINL, 5);
+	PinCheckers[45] = new PinChecker(&PINL, 4);
+	PinCheckers[46] = new PinChecker(&PINL, 3);
+	PinCheckers[47] = new PinChecker(&PINL, 2);
+	PinCheckers[48] = new PinChecker(&PINL, 1);
+	PinCheckers[49] = new PinChecker(&PINL, 0);
+	PinCheckers[50] = new PinChecker(&PINB, 3);
+	PinCheckers[51] = new PinChecker(&PINB, 2);
+	PinCheckers[52] = new PinChecker(&PINB, 1);
+	PinCheckers[53] = new PinChecker(&PINB, 0);
+};
+*/
+
+
+PinChecker& getPinChecker(int pin)
+{
+  if (pin < 0 || pin > 53)
+  {
+    Serial.print("pin out of range: ");
+    Serial.println(pin);
+
+    // return pc 0 if out of range, so the prog doesn't have to do null checks.
+    return PinCheckers[0];  
+  }
+ 
+  return PinCheckers[pin];
+}
+
+
 class Button
 {
   public:
     Button(int pin1, int pin2, char aprefix, char aup, char adown)
-    :buttonPin1(pin1), buttonPin2(pin2), 
+    :buttonPin1(pin1), 
+     pc1(getPinChecker(pin1)),
+     buttonPin2(pin2), 
+     pc2(getPinChecker(pin2)),
     prefix(aprefix), up(aup), down(adown)
     {
      prevState1 = -1;
@@ -27,9 +182,16 @@ class Button
      lastDebounceTime2 = 0;
     }  
 
+  bool HasRegister(volatile unsigned char *aRegister) const 
+  {
+    return (aRegister == pc1.mRegister) || (aRegister == pc2.mRegister);
+  }
+
   char up, down, prefix;
   const int buttonPin1;
   const int buttonPin2;
+  PinChecker &pc1;
+  PinChecker &pc2;
   int prevState1;
   int prevState2;
   long lastDebounceTime1;
@@ -46,7 +208,7 @@ class Button
 
   void DoButtonStuff()
   {  
-    int buttonState1 = digitalRead(buttonPin1);
+    int buttonState1 = pc1.isPinOn();
   
     if (buttonState1 != prevState1) 
     {
@@ -71,7 +233,7 @@ class Button
       prevState1 = buttonState1;
     }
 
-    int buttonState2 = digitalRead(buttonPin2);
+    int buttonState2 = pc2.isPinOn();
     if (buttonState2 != prevState2) 
     {
       if ((millis()-lastDebounceTime2) > debounceDelay)
@@ -79,19 +241,21 @@ class Button
         if (buttonState2 == LOW) 
         {
           unsigned long time2 = micros();
-         Serial.print(prefix);
-         Serial.print(up);
-         Serial.print(" ");
-         Serial.println(time2 - time1);
+          Serial.print(prefix);
+          Serial.print(up);
+          Serial.print(" ");
+          Serial.println(time2 - time1);
         }
-        else {
-         //Serial.print(prefix);
-         //Serial.println(down);
+        else 
+        {
+          //Serial.print(prefix);
+          //Serial.println(down);
         }
       }
-      else {
-         Serial.print(prefix);
-         Serial.println("9B");
+      else 
+      {
+        Serial.print(prefix);
+        Serial.println("9B");
       }
       lastDebounceTime2 = millis();
       prevState2 = buttonState2;
@@ -100,51 +264,130 @@ class Button
 };
 
 Button Buttons[24] = { 
-  /*
-  Button(2, 3, 'K', 'C', 'c'), 
-  Button(3, 4, 'K', 'D', 'd'), 
-  Button(5, 6, 'K', 'E', 'e'), 
-  Button(7, 8, 'K', 'F', 'f'), 
-  Button(9, 10, 'K', 'G', 'g'), 
-  Button(11, 12, 'K', 'H', 'h:'), 
-  */
+  Button(26, 51, 'K', 'A', 'a'), //  0
+  Button(27, 52, 'K', 'B', 'b'), //  1
+  Button(2, 29, 'K', 'C', 'c'),  //  2
+  Button(3, 30, 'K', 'D', 'd'),  //  3
+  Button(4, 31, 'K', 'E', 'e'),  //  4
+  Button(5, 32, 'K', 'F', 'f'),  //  5
+  Button(6, 33, 'K', 'G', 'g'),  //  6
+  Button(7, 34, 'K', 'H', 'h'),  //  7
+  Button(8, 35, 'K', 'I', 'i'),  //  8
+  Button(9, 36, 'K', 'J', 'j'),  //  9
+  Button(10, 37, 'K', 'K', 'k'), //  10
+  Button(11, 38, 'K', 'L', 'l'), //  11 
+  Button(12, 39, 'K', 'M', 'm'), //  12 
+  Button(13, 40, 'K', 'N', 'n'), //  13 
+  Button(14, 41, 'K', 'O', 'o'), //  14
+  Button(15, 42, 'K', 'P', 'p'), //  15
+  Button(16, 43, 'K', 'Q', 'q'), //  16
+  Button(17, 44, 'K', 'R', 'r'), //  17
+  Button(18, 45, 'K', 'S', 's'), //  18
+  Button(19, 46, 'K', 'T', 't'), //  19
+  Button(20, 47, 'K', 'U', 'u'), //  20
+  Button(21, 48, 'K', 'V', 'v'), //  21
+  Button(22, 49, 'K', 'W', 'w'), //  22
+  Button(24, 50, 'K', 'X', 'x')  //  23
+};  
 
-  Button(26, 51, 'K', 'A', 'a'), 
-  Button(27, 52, 'K', 'B', 'b'), 
-  Button(2, 29, 'K', 'C', 'c'), 
-  Button(3, 30, 'K', 'D', 'd'),
-  Button(4, 31, 'K', 'E', 'e'),
-  Button(5, 32, 'K', 'F', 'f'),
-  Button(6, 33, 'K', 'G', 'g'),
-  Button(7, 34, 'K', 'H', 'h'),
-  Button(8, 35, 'K', 'I', 'i'),
-  Button(9, 36, 'K', 'J', 'j'),
-  Button(10, 37, 'K', 'K', 'k'),
-  Button(11, 38, 'K', 'L', 'l'), 
-  Button(12, 39, 'K', 'M', 'm'), 
-  Button(13, 40, 'K', 'N', 'n'), 
-  Button(14, 41, 'K', 'O', 'o'),
-  Button(15, 42, 'K', 'P', 'p'),
-  Button(16, 43, 'K', 'Q', 'q'),
-  Button(17, 44, 'K', 'R', 'r'),
-  Button(18, 45, 'K', 'S', 's'),
-  Button(19, 46, 'K', 'T', 't'),
-  Button(20, 47, 'K', 'U', 'u'),
-  Button(21, 48, 'K', 'V', 'v'),
-  Button(22, 49, 'K', 'W', 'w'),
-  Button(24, 50, 'K', 'X', 'x')
-};
+int InitPinButtonArray(unsigned int *aButtonArray, volatile unsigned char *aRegister)
+{
+  int count = 0;
 
+  for (int i =0; i< sizeof(Buttons)/sizeof(Button); ++i)
+  {
+    if (Buttons[i].HasRegister(aRegister))
+    {
+      aButtonArray[count] = (unsigned int)&(Buttons[i]);
+      ++count;
+    }
+    if (count > 8)
+    {
+      Serial.println("pinbuttonarray count exceeded!!");
+      count = 8;
+      break;
+    }
+  }
+
+  // terminate the array with zero.
+  aButtonArray[count] = 0;
+  
+  return count;
+}
+
+
+/*
+int InitPinButtonArray(Button **aButtonArray)
+{
+  //  aButtonArray[0] = 0;
+
+  // Array[0] = 0;
+  return 0;
+}
+
+int InitPinButtonArray(Button **aButtonArray, volatile unsigned char *aRegister)
+{
+  int count = 0;
+
+  for (int i =0; i< sizeof(Buttons)/sizeof(Button); ++i)
+  {
+    if (Buttons[i].HasRegister(aRegister))
+    {
+      aButtonArray[count] = &(Buttons[i]);
+      ++count;
+    }
+    if (count > 8)
+    {
+      Serial.println("pinbuttonarray count exceeded!!");
+      count = 8;
+      break;
+    }
+  }
+
+  // terminate the array with zero.
+  aButtonArray[count] = 0;
+  
+  return count;
+}
+*/
+
+// all buttons with pins in register A.  (22 - 29)
+Button *AButtons[9];
+Button *BButtons[9];
+Button *CButtons[9];
+Button *DButtons[9];
+Button *EButtons[9];
+Button *FButtons[9];
+Button *GButtons[9];
+Button *HButtons[9];
+Button *JButtons[9];
+Button *LButtons[9];
+
+
+/*
 Button Selector[0] = { 
-/*  Button(30, 'S', 'A', 'a'), 
+  Button(30, 'S', 'A', 'a'), 
   Button(31, 'S', 'B', 'b'), 
   Button(32, 'S', 'C', 'c'), 
   Button(33, 'S', 'D', 'd'),
-  Button(34, 'S', 'E', 'e') */
+  Button(34, 'S', 'E', 'e') 
 };
+*/
 
 void setup()
 {
+  // set up pin button arrays.
+  InitPinButtonArray((unsigned int*)AButtons, &PINA);
+  InitPinButtonArray((unsigned int*)BButtons, &PINB);
+  InitPinButtonArray((unsigned int*)CButtons, &PINC);
+  InitPinButtonArray((unsigned int*)DButtons, &PIND);
+  InitPinButtonArray((unsigned int*)EButtons, &PINE);
+  InitPinButtonArray((unsigned int*)FButtons, &PINF);
+  InitPinButtonArray((unsigned int*)GButtons, &PING);
+  InitPinButtonArray((unsigned int*)HButtons, &PINH);
+  InitPinButtonArray((unsigned int*)JButtons, &PINJ);
+  InitPinButtonArray((unsigned int*)LButtons, &PINL);
+  
   // start serial port at 9600 bps:
   Serial.begin(9600);
   while (!Serial) {
@@ -155,13 +398,13 @@ void setup()
   {
     Buttons[i].InitPin();  
   }
-  
 
+  /*
   for (int i =0; i< sizeof(Selector)/sizeof(Button); ++i)
   {
     Selector[i].InitPin();  
   }
-
+  */
   
 }
 
@@ -169,6 +412,14 @@ int count = 0;
 unsigned long time1 = 0;
 unsigned char oldpina, oldpinb, oldpinc, oldpind, oldpine;
 unsigned char oldpinf, oldping, oldpinh, oldpinj, oldpinl;
+
+inline void checkbuttons( volatile unsigned char &aRegister, 
+                          unsigned char &aOld, 
+                          unsigned int* aButtonArray)
+{
+  if (aOld != aRegister)
+    aOld = aRegister; 
+}
 
 void loop()
 {
@@ -185,9 +436,20 @@ void loop()
   
   ++count;
   
-  /*
+  // is this as fast as the non-inline function code? 
+  checkbuttons(PINA, oldpina, AButtons);
+  checkbuttons(PINB, oldpinb, BButtons);
+  checkbuttons(PINC, oldpinc, CButtons);
+  checkbuttons(PIND, oldpind, DButtons);
+  checkbuttons(PINE, oldpine, EButtons);
+  checkbuttons(PINF, oldpinf, FButtons);
+  checkbuttons(PING, oldping, GButtons);
+  checkbuttons(PINH, oldpinh, HButtons);
+  checkbuttons(PINJ, oldpinj, JButtons);
+  checkbuttons(PINL, oldpinl, LButtons);
+
+/* 
   // takes 167750 for 10000 loops, or 16.7 microseconds.
-  
   if (oldpina != PINA)
     oldpina = PINA;
   if (oldpinb != PINB)
@@ -208,13 +470,15 @@ void loop()
     oldpinj != PINJ;
   if (oldpinl != PINL)
     oldpinl != PINL;
-  */
-
+*/
+  /*
   // takes 3671324 per 10000 loops, or 367 microseconds.
   for (int i =0; i< sizeof(Buttons)/sizeof(Button); ++i)
   {
     Buttons[i].DoButtonStuff();  
   }
+  
+  */
   
   
 /*
