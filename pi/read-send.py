4b9085d31b07865e151136dba8d5e04b60956c66
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2013-08-28 00:12:56 -0600

    added koto, rhodes, tr909

--- pi/read-send.py
+++ pi/read-send.py
@@ -7,7 +7,25 @@ mapping0 = {'A':'/chimes/0',
             'C':'/chimes/2',
             'D':'/chimes/3',
             'E':'/chimes/4',
-            'F':'/chimes/5'}
+            'F':'/chimes/5',
+            'G':'/koto/0',
+            'H':'/koto/1',
+            'I':'/koto/2',
+            'J':'/koto/3',
+            'K':'/koto/4',
+            'L':'/koto/5',
+            'M':'/rhodes/0',
+            'N':'/rhodes/1',
+            'O':'/rhodes/2',
+            'P':'/rhodes/3',
+            'Q':'/rhodes/4',
+            'R':'/rhodes/5'
+            'S':'/tr909/0',
+            'T':'/tr909/1',
+            'U':'/tr909/2',
+            'V':'/tr909/3',
+            'W':'/tr909/4',
+            'X':'/tr909/5'}
 
 mapping = mapping0
 
