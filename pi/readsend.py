7f7a5605af4f10c7b97188973df8ba1fe501b5a6
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2013-09-15 11:53:02 +0000

    fixed pathstr from /fm to /fm/note

--- pi/readsend.py
+++ pi/readsend.py
@@ -4,30 +4,30 @@ import serial, liblo, threading, scales
 
 pd = liblo.Address(8000)
 
-keymapping = {'A':('/fm',0),
-            'B':('/fm',1),
-            'C':('/fm',2),
-            'D':('/fm',3),
-            'E':('/fm',4),
-            'F':('/fm',5),
-            'G':('/fm',6),
-            'H':('/fm',7),
-            'I':('/fm',8),
-            'J':('/fm',9),
-            'K':('/fm',10),
-            'L':('/fm',11),
-            'M':('/fm',12),
-            'N':('/fm',13),
-            'O':('/fm',14),
-            'P':('/fm',15),
-            'Q':('/fm',16),
-            'R':('/fm',17),
-            'S':('/fm',18),
-            'T':('/fm',19),
-            'U':('/fm',20),
-            'V':('/fm',21),
-            'W':('/fm',22),
-            'X':('/fm',23)}
+keymapping = {'A':0,
+            'B':1,
+            'C':2,
+            'D':3,
+            'E':4,
+            'F':5,
+            'G':6,
+            'H':7,
+            'I':8,
+            'J':9,
+            'K':10,
+            'L':11,
+            'M':12,
+            'N':13,
+            'O':14,
+            'P':15,
+            'Q':16,
+            'R':17,
+            'S':18,
+            'T':19,
+            'U':20,
+            'V':21,
+            'W':22,
+            'X':23}
 
 knobmapping = {'A':'/delay/time',
                 'B':'/fm/harmonic',
@@ -65,7 +65,7 @@ def velocurve(t):
 ##########
 
 def handleMsg(msg):
-    global keymapping, synth
+    global scale
     print msg
     head = msg[0]
     if head=='#':
@@ -88,12 +88,9 @@ def handleMsg(msg):
         liblo.send(pd, pathstr)
     else:
         t = float(msg[1:])
-        if synth:
-            pathstr, midi = keymapping[head]
-            liblo.send(pd, pathstr, midi, velocurve(t))
-        else:
-            pathstr = keymapping[head]
-            liblo.send(pd, pathstr, velocurve(t))
+        index = keymapping[head]
+        midi = key + scale(index)
+        liblo.send(pd, '/fm/note', midi, velocurve(t))
 
 ############
 
@@ -108,7 +105,7 @@ class ArduinoThread(threading.Thread):
             try:
                 handleMsg(self.arduino.readline())
             except:
-                pass # lol
+                print 'Something bad happened.'
 
 #######
 
