b77eb4c7b8719b5f16f95ef96d631e0e009087ae
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2013-11-21 00:15:40 -0700

    new rosettes

--- design/rosette.py
+++ design/rosette.py
@@ -8,7 +8,7 @@ cyclo_radius = 20.5 # inches
 
 thetas = []
 for i in range(narms):
-    thetas.append(np.arange(0,2*np.pi+0.1,2*np.pi/24.)+i*2*np.pi/narms)
+    thetas.append(np.arange(0,2*np.pi+0.001,2*np.pi/128.)+i*2*np.pi/narms)
 
 r = np.exp(alpha*thetas[0])-1
 
@@ -21,11 +21,18 @@ for rr in r:
 for m in measurements:
     print m
 
-for theta in thetas:
-    pylab.polar(theta, r, color='k', linewidth=2.5)
-    pylab.polar(-theta, r, color='k', linewidth=2.5)
 
+
+fig = pylab.figure()
+ax = fig.add_subplot(111, polar=True)
+ax.grid(False)
+for theta in thetas:
+    ax.plot(theta, r, color='k', linewidth=2.5)
+    ax.plot(-theta, r, color='k', linewidth=2.5)
+
+#pylab.grid(False)
 pylab.rgrids([1.], labels=None)
-pylab.thetagrids(np.arange(0., 360., 360./24), labels=None)
+#pylab.thetagrids(np.arange(0., 360., 360./24), labels=None)
+#pylab.grids(False)
 
 pylab.show()
