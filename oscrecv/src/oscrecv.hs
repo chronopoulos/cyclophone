c45b2c659e5c444569adbefc5d4f5f6f9793ad84
B <bburdette@parvis>
2014-03-22 13:41:03 -0600

    add port arg to oscrecv

--- oscrecv/src/oscrecv.hs
+++ oscrecv/src/oscrecv.hs
@@ -10,10 +10,14 @@ main =
   in void (forkIO (withTransport t f))
 -}
 
-main = 
-  let {f fd = forever (recvMessage fd >>= print)
-     ;t = udpServer "127.0.0.1" 57300}
-  in 
-    withTransport t f
+getArgsPort args = 
+ if (length args) < 1 
+   then 9000
+   else (read (head args))
 
-
+main = do
+  args <- getArgs
+  withTransport (t (getArgsPort args)) f
+  where
+    f fd = forever (recvMessage fd >>= print)
+    t port = udpServer "127.0.0.1" port 
