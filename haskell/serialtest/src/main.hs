b43e3323a97c8c9ef4261dcd9fd22462150a0832
B <bburdette@gmail.com>
2014-05-27 14:57:47 -0600

    works with the arduino now!

--- haskell/serialtest/src/main.hs
+++ haskell/serialtest/src/main.hs
@@ -1,26 +1,34 @@
 import System.IO
 import System.Environment
 import System.Hardware.Serialport
+import qualified Data.ByteString.Char8 as B
 
 -- let port = "COM3"           -- Windows
 -- let port = "/dev/ttyUSB0"   -- Linux
 
-printLines h = do
-  line <- hGetLine h
+printLines serial = do
+  line <- getaline serial
   print line
-  printLines h
+  printLines serial
+
+getaline :: SerialPort -> IO B.ByteString
+getaline serial = do
+  c <- recv serial 1
+  if c == B.pack "\n"
+    then return $ B.pack ""
+    else do 
+      rest <- getaline serial
+      return $ B.append c rest 
 
 main =
   do
     args <- getArgs
-    if (length args) < 4
+    if (length args) < 1
       then do 
         putStrLn "serialtest requires at least 1 arg:"
         putStrLn "serialtest <com port>"
         putStrLn "example: 'serialtest /dev/ttyACM0'"
       else do
-        h <- hOpenSerial (head args) defaultSerialSettings
-        printLines h     
-        -- hGetLine h >>= print
-        -- hPutStr h "AT\r"
-        -- hClose h
+        serial <- openSerial (head args) 
+                (defaultSerialSettings { commSpeed = CS115200 })
+        printLines serial     
