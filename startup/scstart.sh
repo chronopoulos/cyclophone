837a46741d5014b139a399f8c041a354ac2e3049
ccrma <ccrma@cyclophone-audio>
2014-06-20 14:47:03 +0000

    supercollider startup for pi, copied from web.

---
+++ startup/scstart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+jackd -m -p 32 -d dummy &
+sleep 1
+alsa_out -q1 2>&1 > /dev/null &
+sleep 1
+scsynth -u 57110 &
+sleep 1
+jack_connect SuperCollider:out_1 alsa_out:playback_1 &
+jack_connect SuperCollider:out_2 alsa_out:playback_2 &
