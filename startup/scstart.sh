ad3f83ec45c1a7de5c6c94544169b0f2ee1b3b1a
ccrma <ccrma@cyclophone-audio>
2014-06-23 04:04:24 +0000

    debugging; change from playing samples to sinoscs for test.

--- startup/scstart.sh
+++ startup/scstart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#jackd -m -p 32 -d dummy --rate 22050 &
 jackd -m -p 32 -d dummy &
 sleep 1
+#alsa_out -d hw:1,0 -r 22050 -q1 2>&1 > /dev/null &
 alsa_out -d hw:1,0 -q1 2>&1 > /dev/null &
 sleep 1
 scsynth -u 57110 &
