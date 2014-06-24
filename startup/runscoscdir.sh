eb6a86b4effa3ee0b900f57367cb04125e910462
ccrma <ccrma@cyclophone-audio>
2014-06-24 15:04:32 +0000

    more dirs.

--- startup/runscoscdir.sh
+++ startup/runscoscdir.sh
@@ -1,3 +1,4 @@
 #screen -d -m scoscdir 192.168.123.144 8000 "blah" ~/cyclophone_samples/mellotron/CombinedChoir/
 #screen -d -m scoscdir 192.168.123.144 8000 "blah" ~/cyclophone_samples/mellotron/M300B/
-screen -d -m scoscdir 192.168.123.144 8000 "blah" ~/cyclophone_samples/mellotron/MkIIFlute/
+#screen -d -m scoscdir 192.168.123.144 8000 "blah" ~/cyclophone_samples/mellotron/MkIIFlute/
+screen -d -m scoscdir 192.168.123.144 8000 "blah" ~/cyclophone_samples/mellotron/StringSection/
