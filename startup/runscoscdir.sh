88ef90da6c43177be4014a50a62d2e6466b231a5
Ben Burdette <bburdette@cyclophone-audio>
2014-08-03 02:21:11 +0000

    use map file in cyclophone_samples dir.

--- startup/runscoscdir.sh
+++ startup/runscoscdir.sh
@@ -1,1 +1,2 @@
-screen -d -m scoscdir 192.168.123.144 8000 ~/cyclophone/haskell/scoscdir/mmapfile.hs
+# screen -d -m scoscdir 192.168.1.143 8000 ~/cyclophone_samples/mmap.hs 
+scoscdir 192.168.1.143 8000 ~/cyclophone_samples/mmap.hs 
