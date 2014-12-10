091ecdfe0276e50c349ac53aed73ad4faa35525d
Ben B <bburdette@parvis>
2014-12-10 21:05:48 +0000

    prefix haskell executables with ~/.cabal/bin/ works for oscadaleds but scoscdir still doesn't work, at least not on boot.

--- startup/bananapi_startscoscdir.sh
+++ startup/bananapi_startscoscdir.sh
@@ -1,4 +1,9 @@
 #screen -d -m ~/.cabal/bin/scoscdir 192.168.8.121 8000 ~/cyclophone_samples/mmap.hs
 #screen -d -m ~/.cabal/bin/scoscdir 192.168.8.174 8000 ~/cyclophone_samples/sine.hs
 #screen -d -m ~/.cabal/bin/scoscdir 127.0.0.1 8000 ~/cyclophone_samples/mmap.hs
-screen -d -m scoscdir 127.0.0.1 8000 ~/cyclophone_samples/mmap.hs 127.0.0.1 8086
+#screen -d -m '~/.cabal/bin/scoscdir 127.0.0.1 8000 ~/cyclophone_samples/mmap.hs 127.0.0.1 8086 2>&1 /home/pi/scoscdir1.txt'
+# '~/.cabal/bin/scoscdir 127.0.0.1 8000 ~/cyclophone_samples/mmap.hs 127.0.0.1 8086 &> ~/scoscdir1.txt'
+#'~/.cabal/bin/scoscdir 127.0.0.1 8000 ~/cyclophone_samples/mmap.hs 127.0.0.1 8086'
+#screen -d -m ~/.cabal/bin/scoscdir 127.0.0.1 8000 ~/cyclophone_samples/mmap.hs 127.0.0.1 8086 &> ~/scoscdir1.txt
+
+screen -d -m ~/.cabal/bin/scoscdir 127.0.0.1 8000 ~/cyclophone_samples/mmap.hs 127.0.0.1 8086
