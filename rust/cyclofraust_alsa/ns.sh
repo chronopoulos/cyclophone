ce16e9554fec3aae09bf0f309bd5cfa18b423c30
Ben B <bburdette@gmail.com>
2016-05-07 20:49:09 -0600

    remove other cyclofraust versions - now are branches in cyclofraust repo

--- rust/cyclofraust_alsa/ns.sh
+++
@@ -1,3 +0,0 @@
-nix-shell -p stdenv coreutils makeWrapper pkgconfig clang llvm emscripten openssl libsndfile libmicrohttpd
-
-# nix-shell -p stdenv coreutils fetchurl makeWrapper pkgconfig clang llvm emscripten openssl libsndfile libmicrohttpd
